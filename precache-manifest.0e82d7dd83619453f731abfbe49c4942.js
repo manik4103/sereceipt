502ab350d8435c74b41562af1797882de0afa2cc
Manikandan <84866071+manik4103@users.noreply.github.com>
2024-11-17 09:20:34 +0800

    initial commit

---
+++ precache-manifest.0e82d7dd83619453f731abfbe49c4942.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77a02bb30e05eb02252002d63849dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed45ac8048a188835602",
+    "url": "/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "dbb03d843025108f5e66",
+    "url": "/static/css/main.415cb014.chunk.css"
+  },
+  {
+    "revision": "ed45ac8048a188835602",
+    "url": "/static/js/2.48581742.chunk.js"
+  },
+  {
+    "revision": "c5f92507b76b3fbc3f8e5f5fe0c84cf3",
+    "url": "/static/js/2.48581742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb03d843025108f5e66",
+    "url": "/static/js/main.577142ae.chunk.js"
+  },
+  {
+    "revision": "c8d6aca6dda5a378a01d",
+    "url": "/static/js/runtime-main.6ee9ec60.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/static/media/resize.d4da359f.svg"
+  }
+]);
